cd10d1ca1fe472f9b17b8d0d58a99ea2a0dc54f1
Shahid Alam <alam.shahid.ds@gmail.com>
2024-12-25 12:38:31 +0530

    react

--- 14 React/mysite/src/App.jsx
+++ 14 React/mysite/src/App.jsx
@@ -1,74 +1,68 @@
-import './App.css';
+import "./App.css";
 
-//definfing new com
-const Greeting = () =>{
-  return <h1>Hello,Jon</h1>
-}
-
-//using dynamic  
-const name ="shahid"
-const Dynamic = () =>{
-  return <h1>Hello , {name}!</h1>
-}
-
-
-// Define the MyComponent component
-const MyComponent = () => {
-  return <h1>Hello JS</h1>; // Return a heading ignore () if its one line
+// 1. Create a React component called Heading, to display a heading as "React Playground".
+const Heading = () => {
+  return <h1>React Playground</h1>;
 };
 
+// 2. Create a React component called Greetings, to display a heading as "Hello, Alice Smith!"
+// where Alice Smith is a dynamic data.
+const name = "Alice Smith";
+const Greeting = () => {
+  return <h1>Hello, {name}!</h1>;
+};
 
-//link 
-const LinkTag = () =>{
-  const url = "http://exmaple.com"
-  return <a href={url} target='_blank'>Visit Exmaple </a>
-}
+//3. Create a React component called ExternalLink, to display the link text "Visit React Examples".
+//Link the text to https://www.reactexamples.com and upon clicking the link it should open in a new tab.
 
-//dislpay image
+const ExtrnalLink = () => {
+  return (
+    <a href="http://reactexamples.com" target="_blank">
+      Visit React Examples
+    </a>
+  );
+};
 
-const ImageTag = () => {
-  const imageUrl = "http://via.placeholder.com/150"
-  return(
-  <div>
-    <img src={imageUrl} alt ="placehioldeR" />
-  </div>
-  )
-}
+//4. Create a React component called Avatar, to display a placeholder image of 200x200.
 
-// profile detail 
-const ProfileDetail = () =>{
-  const details = {
-    name : "john",
-    age :25,
-    email : "john@gmail.com",
-    bio : "Hey im looking for  job"
-  }
+const Avatar = () => {
+  return (
+    <div>
+      <img src="https://via.placeholder.com/200x200" alt="avatar" />
+    </div> 
+    )
+};
+
+//5. Create a React component called UserInfo to display the following user info.
+
+const UserInfo = () =>{
+  const user = {
+      name: "Alice Smith",
+      age: 28,
+      email: "alice.smith@example.com",
+      bio: "Passionate about React and building innovative web applications."
+    };
 
   return (
     <div>
-      <h2>Profile Details</h2>
-      <p>Name : {details.name}</p>
-      <p>Age : {details.age}</p>
-      <p>email : {details.email}</p>
-      <p>Bio : {details.bio}</p>
+      <h1>User Information</h1>
+      <p><strong> Name: </strong>{user.name} </p>
+      <p><strong> Age: </strong>{user.age} </p>
+      <p><strong> Email: </strong>{user.email} </p>
+      <p><strong> Bio: </strong>{user.bio} </p>
     </div>
   )
 }
 
-// Define the App component
 function App() {
   return (
-    <div>
-      {/* Rendering JSX */}
-      <MyComponent />
+    <>
+      <Heading />
       <Greeting />
-      <Dynamic />
-      <LinkTag />
-      <ImageTag />
-      <ProfileDetail />
-    </div>
+      <ExtrnalLink />
+      <Avatar />
+      <UserInfo />
+    </>
   );
 }
-
-// Export the App component at the end
 export default App;
