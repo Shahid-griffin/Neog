1e2416e33964d9ca5905ae6d1163971f32db4aa9
Shahid Alam <alam.shahid.ds@gmail.com>
2024-12-25 20:07:08 +0530

    react

---
+++ 14.1 React_HW_CW/13_IR1.5.jsx
@@ -0,0 +1,55 @@
+import "./App.css";
+
+const Addition = () => {
+  const a = 2;
+  const b = 5;
+  return (
+    <p>
+      The result of {a} + {b} is {a + b}
+    </p>
+  );
+};
+
+const TemperatureCalculator = () => {
+  const celcius = 30;
+  const fahrenheit = (celcius * 9) / 5 + 32;
+  return (
+    <p>
+      {celcius} degree is equal to {fahrenheit} Fahrenheit
+    </p>
+  );
+};
+
+const CombinedOperation = () => {
+  const a = 15;
+  const b = 7;
+  const c = 3;
+  return (
+    <div>
+      <p>
+        {a} + {b} equal to {a + b}
+      </p>
+      <p>
+        {a} + {b} + {c} is equal to {a + b + c}
+      </p>
+    </div>
+  );
+};
+
+const Welcome = () => {
+  const firstName = "john";
+  const lastName = "Doe";
+
+  return <p>{`Welcome ,${firstName} ${lastName}`}</p>;
+};
+
+export default function App() {
+  return (
+    <main>
+      <Addition />
+      <TemperatureCalculator />
+      <Welcome />
+      <CombinedOperation />
+    </main>
+  );
+}
