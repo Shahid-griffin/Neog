a9da7ca1ff5e21c33f03d76ee3c2e8f41f9477be
Shahid Alam <alam.shahid.ds@gmail.com>
2024-12-26 11:49:34 +0530

    react

---
+++ 14.1 React_HW_CW/16_IR1.6.jsx
@@ -0,0 +1,48 @@
+import "./App.css";
+
+const MathOperation = () => {
+  const a = 20;
+  const b = 8;
+
+  const getSum = (x, y) => {
+    return x + y;
+  };
+
+  const getDifference = (x, y) => {
+    return x - y;
+  };
+  return (
+    <div>
+      <p>
+        {a} + {b} equal {getSum(a, b)}
+      </p>
+      <p>
+        {a} - {b} equal {getDifference(a, b)}
+      </p>
+    </div>
+  );
+};
+
+const CallGreetingFunction = () => {
+  const getGreeting = () => {
+    return "Hello World";
+  };
+  return <h1>{getGreeting()}</h1>;
+};
+
+const GetFormattedDAte = () => {
+  const getCurrentDate = () => {
+    return new Date().toLocaleDateString();
+  };
+  return <p>Current Date: {getCurrentDate()}</p>;
+};
+
+export default function App() {
+  return (
+    <>
+      <GetFormattedDAte />
+      <CallGreetingFunction />
+      <MathOperation />
+    </>
+  );
+}
