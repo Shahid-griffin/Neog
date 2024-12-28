8566ede2ff915962a9c985b28135b7ca1344f899
Shahid Alam <alam.shahid.ds@gmail.com>
2024-12-28 17:22:12 +0530

    react

---
+++ 14.1 React_HW_CW/34_3.1CW1/pages/Blog.jsx
@@ -0,0 +1,47 @@
+function Blog() {
+  return (
+    <main className="container py-4">
+      <h1>Blogs</h1>
+
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card mt-3">
+            <div className="card-body">
+              <h5>Blog 1</h5>
+              <p className="card-text">
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s,
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className=" col-md-6">
+          <div className="card mt-3">
+            <div className="card-body">
+              <h5>Blog 2</h5>
+              <p className="card-text">
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s,
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className=" col-md-6">
+          <div className="card mt-3">
+            <div className="card-body">
+              <h5>Blog 3</h5>
+              <p className="card-text">
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s,
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+export default Blog;
---
+++ 14.1 React_HW_CW/35_3.1_HW1/App.jsx
@@ -0,0 +1,27 @@
+export default function App() {
+    return (
+      <main className="container py-4">
+        <h1 className="display-3">Featured Restraurants</h1>
+        <img
+          src="https://images.pexels.com/photos/1581554/pexels-photo-1581554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="img-fluid rounded py-4"
+          alt="image 1"
+        />
+  
+        <h3>Taste of Italy</h3>
+        <p>Italian | Rating 4.5</p>
+        <p>123 Main Street,Anytown, USA</p>
+  
+        <img
+          src="https://images.pexels.com/photos/239975/pexels-photo-239975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="img-fluid rounded  py-4"
+          alt="image 2"
+        />
+  
+        <h3>Spice Village</h3>
+        <p>India | Rating 4.3</p>
+        <p>456 Main Street,Anytown, USA</p>
+      </main>
+    );
+  }
+  
